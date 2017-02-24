9db65faa84b6c679a8dcc22aaaa7cff18f2bdf6d
Piao Piao <94313353@qq.com>
2017-02-24 11:03:09 +0800

    Initial Commit

---
+++ testVariableHeightScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testVariableHeightScrollView
+//
+//  Created by Piao Piao on 2017/2/24.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testVariableHeightScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testVariableHeightScrollView
+//
+//  Created by Piao Piao on 2017/2/24.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
